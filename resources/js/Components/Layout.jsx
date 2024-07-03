6703298d9331525722492a300ac99c2ca29efb25
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2024-07-04 01:50:21 +0600

    added admin button

--- resources/js/Components/Layout.jsx
+++ resources/js/Components/Layout.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { FiLogOut } from "react-icons/fi";
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
+import { BiUser } from "react-icons/bi";
 
 const Layout = ({ children }) => {
+    const { auth } = usePage().props;
+    console.log(auth);
     return (
         <div>
             <Navbar />
-            <div
-                className="bg-main"
-                style={{ minHeight: "89vh" }}
-            >
+            <div className="bg-main" style={{ minHeight: "89vh" }}>
                 {children}
                 <Footer />
             </div>
+            {auth.is_admin && (
+                <a
+                    href="/admin"
+                    className="position-fixed"
+                    style={{
+                        bottom: "100px",
+                        right: "20px",
+                        zIndex: "1000",
+                    }}
+                >
+                    <button className="btn btn-primary">
+                        <BiUser />
+                    </button>
+                </a>
+            )}
             <Link
                 href="/logout"
                 className="position-fixed"
